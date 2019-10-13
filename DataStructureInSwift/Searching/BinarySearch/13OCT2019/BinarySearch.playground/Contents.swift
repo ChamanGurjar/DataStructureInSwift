cada520b35ea81e1623ec926a172591ff20339f6
Chaman Gurjar <chamangujjar162@gmail.com>
2019-10-13 08:25:04 +0530

    Binary search playground created.

---
+++ DataStructureInSwift/Searching/BinarySearch/13OCT2019/BinarySearch.playground/Contents.swift
@@ -0,0 +1,16 @@
+//
+//  BinarySearch.playground
+//  DataStructureInSwift
+//
+//  Created by Chaman Gurjar on 13/10/19.
+//  Copyright © 2019 Chaman Gurjar. All rights reserved.
+//
+
+import UIKit
+
+/** IMPORTANT NOTE
+ Binary search works because the input array is sorted. If the input array isn’t sorted, binary search won’t work. Moreover, binary search also only works for array values that can be compared, such as numbers.
+ */
+
+
+var str = "Hello, playground"
