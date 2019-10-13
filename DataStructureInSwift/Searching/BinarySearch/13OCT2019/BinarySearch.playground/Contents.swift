61a5f0cb8b0fdb5e7d51c0cb371929985e87d3fb
Chaman Gurjar <chamangujjar162@gmail.com>
2019-10-13 12:33:51 +0530

    Binary search implemented.

--- DataStructureInSwift/Searching/BinarySearch/13OCT2019/BinarySearch.playground/Contents.swift
+++ DataStructureInSwift/Searching/BinarySearch/13OCT2019/BinarySearch.playground/Contents.swift
@@ -14,3 +14,31 @@ import UIKit
 
 
 var str = "Hello, playground"
+
+private func binarySearch(in numbers: [Int], for value: Int) -> Int? {
+    var startIndex = 0
+    var endIndex = numbers.count - 1
+    
+    while startIndex <= endIndex {
+        let middleIndex = Int(floor(Double(startIndex) + Double(endIndex - startIndex)/2.0))
+        // print(middleIndex)
+        let middleValue = numbers[middleIndex]
+        if middleValue == value {
+            return middleIndex
+        } else if middleValue < value {
+            startIndex = middleIndex + 1
+        } else {
+            endIndex = middleIndex - 1
+        }
+    }
+    
+    return nil
+}
+
+let numbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
+let value = 13
+if let index = binarySearch(in: numbers, for: value) {
+    print("Found \(value) at index \(index)")
+} else {
+    print("Did not find \(value)")
+}
